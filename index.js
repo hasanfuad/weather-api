bfbdfb90c907b2a1129ef46b3e22c5a5143e60ec
Fuad Hasan <hasanmdfuad@hotmail.com>
2020-08-14 21:17:58 +0600

    modified

--- index.js
+++ index.js
@@ -1,104 +1,48 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const https = require("https");
 const app = express();
 
-app.get("/", function(req, res)
-{   
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=33c14bacfecc1e313fdc3a8f6b72bba0&units=metric"
+app.use(bodyParser.urlencoded({extended: true}));
 
-    https.get(url, function(response){
-        response.on("data", function(data){
-            const weather = JSON.parse(data);
-            const weatherData = weather.main.temp;
-            res.send("This is the temp: "+weatherData)
-        })
-    })
-    
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
 })
 
-
-
-app.listen(3000, function(){
-    console.log("server is running!!");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-
-// const https = require("https");
-// const { json } = require("express");
-// const bodyParser = require("body-parser");
-// const { urlencoded } = require("body-parser");
-// const app = express();
-
-// app.use(bodyParser.urlencoded({extended: true}));
-
-
-
-// app.get("/", function(req, res){
+app.post("/", function(req, res){
+    
     
 
-//     res.sendFile(__dirname+"/index.html");
+        const query = req.body.input;
+        const apiKey = "33c14bacfecc1e313fdc3a8f6b72bba0";
+        const unit = "metric";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit+"";
+    
+            https.get(url, function(response){
+                response.on("data", function(data){
+                    const weatherData = JSON.parse(data);
+                    const temp = weatherData.main.temp;
+                    const des = weatherData.weather[0].description;
+                    const icons = weatherData.weather[0].icon
+                    const imgURL = "https://openweathermap.org/img/wn/"+icons+"@2x.png";
+    
+                        res.write("<h1 style =  color:red> <span style= color:lightGreen> "+query+"</span> Temp is "+temp+"</h1>");
+                        res.write("<h2 style = color:cyan>"+des+"</h2>");
+                        res.write("<img src="+imgURL+">");
+    
+                        res.send();
+                })
+            })
 
-// })
+})
 
-// app.post("/", function(req, res){
-//     console.log(req.body.input);
+app.get("/", function(req, res)
+{   
+    
+})
+        app.listen(3000, function(){
+            console.log("Server is running!");
+        })
 
-//     const cityName = req.body.input;
-//     const apiKey = "33c14bacfecc1e313fdc3a8f6b72bba0";
-//     const unit = "metric";
-//     const url ="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey+"&units="+unit+"";
-//     https.get(url, function(response){
-
-//     response.on("data", function(data){
-//         const weather = JSON.parse(data);
-//         const weatherData = weather.main.temp;
-//         const weatherDes = weather.weather[0].description;
-//         const icon = weather.weather[0].icon;
-//         const imgURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
-
-//         res.write("<h1>The current temp in "+cityName+" is "+weatherData+" celcius</h1>");
-//         res.write("<p>"+weatherDes+"</p>")
-//         res.write("<img src="+imgURL+">")
-//         res.send()
-//     })
-// })
-// })
-
-// app.listen(3000, function(){
-//     console.log("server is running");
-// })
-
-
-
-
+        
